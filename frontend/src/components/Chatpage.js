dab63e0eb0547f9576d8b2ae4f0bcfa9a87ba260
LOVELELESS <koh_rayson@yahoo.com.sg>
2020-02-16 17:59:04 +0800

    added basic form to enter room, passed props from homepage to chatpage

--- frontend/src/components/Chatpage.js
+++ frontend/src/components/Chatpage.js
@@ -4,6 +4,8 @@ import {ENDPOINT} from '../configs';
 const socket = io(ENDPOINT);
 
 const Chatpage = props => {
+  const {name, room} = props.location;
+  console.log(name, room);
   return <h1>CHAT PAGE</h1>;
 };
 
--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Homepage = props => {
-  return <h1>HOME PAGE</h1>;
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  const handleEnterRoom = e => {
+    if (name === '' || room === '') {
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="name?"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <input
+        placeholder="room?"
+        value={room}
+        onChange={e => setRoom(e.target.value)}
+      />
+      <Link to={{pathname: '/chat', name, room}} onClick={handleEnterRoom}>
+        <button>enter room</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Homepage;
